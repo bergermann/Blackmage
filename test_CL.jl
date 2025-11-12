eeffd084de119a72c7946424bcd7aff5cd53cfba
Dominik Bergermann <dominik.bergermann@gmx.de>
2025-11-12 19:54:17 +0100

    added closed loop tests

--- test_CL.jl
+++ test_CL.jl
@@ -1,18 +1,20 @@
 
 using Blackmage, JLD2
 
+# use julias help mode to read information of each function
 
+# connect to devices, insert respective IP and port number
+#  MC -> motor control
+# IDS -> Interferometric Displacement Sensor
+# IDS web interface can be opened by putting the IP adress into any browser
+device_mc = connect(ip"",0)     # motors
+device_ids = connect(ip"",0)    # interferometers
 
-device_mc = connect(ip"",0)
-device_ids = connect(ip"",0)
-
+# run this to close connection
 close(device_mc); close(device_ids)
 
-
-
-# mcMove.(device_mc,[1,2,3],0,0)
-# mcStop.(device_mc,[1,2,3])
-
+# set all motors to external drive mode
+# maxdist and target tolerance are given in IDS units
 mcSetupFCM(device_mc;
     master=1,
     stepsize=100,
@@ -22,37 +24,42 @@ mcSetupFCM(device_mc;
     freqslave=70,
     temp=300)
 
-mcStatusFCM(device_mc)
-
-# mcDisableFCM(device_mc)
+# stop all motors and set them back to direct drive mode
 mcStopAll(device_mc)
 
+# set target distance for all three motors
+# this needs to be done once ideally at the positions where interferometers were initialized
+# distance can not go lower than 4_500_000 IDS units, therefore ideally initialize IDS
+# as close to backplate as possilbe
 mcTargetFCM(device_mc,0,:cm)
-mcTargetFCM(device_mc,1,:cm)
-mcTargetFCM(device_mc,2,:cm)
 
+# after initialization freely set target positions
+mcTargetFCM(device_mc,1,:cm)
+
+# block program until target is reached
 mcWaitForTarget(device_mc)
 
-# metric2ids(0,:nm)
 
 
-d = Displacement(100_000)
-d_DL = Displacement(100_000)
-d.t0 = d_DL.t0 = now()
+# example procedure to test positioning reliability
 
-record!(d,device_ids,1_000)
+d = Displacement(100_000)       # record IDS measurement here
+d_DL = Displacement(100_000)    # record internal MC position here
+d.t0 = d_DL.t0 = now()          # synchronize timestamps
 
-d_m = zeros(3,20)
-d_s = zeros(3,20)
+record!(d,device_ids,10*60)
+
+d_m = zeros(3,20)               # repeatedly measure position after each step
+d_s = zeros(3,20)               # with uncertainties
 
 for i in 1:10
     println("Iter $i")
-    mcTargetFCM(device_mc,1,:cm)
-    mcWaitForTarget(device_mc,d_DL)
-    # mcWaitForTarget(device_mc)
+
+    mcTargetFCM(device_mc,1,:cm)    
+    mcWaitForTarget(device_mc,d_DL) # record internal positions while moving
     sleep(1)
 
-    stop_record!(d)
+    stop_record!(d)                 # stop main recording to do precision measurement
 
     sleep(1)
 
@@ -63,7 +70,6 @@ for i in 1:10
 
     mcTargetFCM(device_mc,2,:cm)
     mcWaitForTarget(device_mc,d_DL)
-    # mcWaitForTarget(device_mc)
     sleep(1)
 
     stop_record!(d)
@@ -76,11 +82,11 @@ for i in 1:10
     sleep(1)
 end
 
-f = "data4.jld2"; if !isfile(f); @save f d d_DL d_m d_s; else; @warn "File already exists."; end
+
+
+# plot and safe plots and data
 
 y = collect(1:div(size(d_m,2),2))
-
-
 p1 = plot(d_m[1,1:2:end]/1e12/1e-3,y; seriestype=:scatter,
     xlabel="Displacement [mm]",ylabel="Iterations",label="Motor 1")
 plot!(d_m[2,1:2:end]/1e12/1e-3,y.+1; xerror=d_s[2,1:2:end]/1e12/1e-3,seriestype=:scatter,label="Motor 2")
@@ -95,8 +101,17 @@ plot!(d_m[3,2:2:end]/1e12/1e-3,y.+2; xerror=d_s[3,2:2:end]/1e12/1e-3,seriestype=
 p3 = histogram(d_m[1,1:2:end]/1e12/1e-3; bins=50,xlabel="Displacement [mm]")
 p4 = histogram(d_m[1,2:2:end]/1e12/1e-3; bins=50,xlabel="Displacement [mm]")
 
+P = [p1,p2,p3,p4]
 
-savefig(p1,"plots/f1.svg")
-savefig(p2,"plots/f2.svg")
-savefig(p3,"plots/f3.svg")
-savefig(p4,"plots/f4.svg")
+
+f = "data2"
+for i in eachindex(P)
+    if isfile("$f.jld2")
+        @warn "Plot file already exists."
+    else
+        savefig(P[i],"plots/$(f)_$i.svg")
+    end
+end
+
+if !isfile("$f.jld2"); @save f d d_DL d_m d_s; else; @warn "File already exists."; end
+
