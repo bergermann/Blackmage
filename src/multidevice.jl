97ad11c57556c37456c7b5b48e474c6debe93824
Dominik Bergermann <dominik.bergermann@gmx.de>
2025-12-07 21:56:59 +0100

    multidevice start

--- src/multidevice.jl
+++ src/multidevice.jl
@@ -1,9 +1,28 @@
 
-const MD = Base.ImmutableDict{Int,IPv4}
+struct MultiDevice
+    mc::Dict{Int,IPv4}
+    ids::Dict{Int,IPv4}
+
+    function MultiDevice(mc,ids)
+        @assert length(mc) == length(ids) && all(k->in(k,keys(ids)),keys(mc))
+            "mc and ids addresses need matching keys."
+
+        new(mc,ids)
+    end
+
+    function MultiDevice(mc::AbstractArray{IPv4},ids::AbstractArray{IPv4})
+        @assert length(mc) == length(ids) "Need matching amount of mc and ids addresses."
+
+        new(
+            Dict(i=> mc[i] for i in eachindex(mc)),
+            Dict(i=>ids[i] for i in eachindex(ids)),
+        )
+    end
+end
+
+const MD = MultiDevice
+
+import Base.getindex
+Base.getindex(md::MultiDevice,idx) = (md.mc[idx],md.ids[idx])
 
 
-
-
-Base.ImmutableDict(
-    1 => ip"192.168.1.1"
-)
